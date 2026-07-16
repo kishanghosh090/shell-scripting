b81de3063a817c96198aa12331d308006bf71fff
kishanghosh090 <kishanghosh090@gmail.com>
2026-07-17 00:19:00 +0530

    add arrs

--- rivison/arr.sh
+++ rivison/arr.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 myArr=( 11 3 34 344 kishan )
 
-echo "${myArr[0]}"
-echo "$myArr"
-echo "${myArr[1]}"
-echo "${myArr[2]}"
-echo "${#myArr[*]}"
+# echo "${myArr[0]}"
+# echo "$myArr"
+# echo "${myArr[1]}"
+# echo "${myArr[2]}"
+# echo "${#myArr[*]}"
+shell=$SHELL
+echo "${myArr[*]:1} $shell"
