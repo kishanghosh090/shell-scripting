3ca3db285d71755c7d09028acfe3f14c5acff300
kishanghosh090 <kishanghosh090@gmail.com>
2026-07-16 23:58:49 +0530

    add vars rivison

--- rivison/vars.sh
+++ rivison/vars.sh
@@ -1,9 +1,26 @@
-#!/bin/zsh
+#!/bin/bash
 
-greet_user() {
-    echo "$@"
-    echo "$0"
-}
-var=$@
+# greet_user() {
+#     echo "$@"
+#     echo "$0"
+# }
+# var=$@
+# newVar=$(hostname)
+# echo "$newVar"
+# greet_user $var
 
-greet_user $var
+# bash
+# <<comments
+# hello
+# is multi line
+# comment
+# comments
+
+#zsh
+: << 'COMMENT'
+
+hello from zsh multi line comments
+COMMENT
+# myvar=(1 23 34 kishan)
+var=$(cat varss.sh)
+echo "$var"
