eb61674a21852817e8c7c2676cd76d8fb5b470d7
kishanghosh090 <kishanghosh090@gmail.com>
2026-07-18 21:35:36 +0530

    add control flow

--- rivison/conditionls.sh
+++ rivison/conditionls.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-if [ 10 -gt 5 ]
+if (( 10 > 5 ))
 then
     echo "10"
 else 
     echo "5"
 fi    
+
+
+# case
+
+
+case a in
+    a) date;;
+    b) ls;;
+    *) echo "not match!!"
+esac    
