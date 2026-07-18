cdeec2f6a75c309b190044e83247d13c386a23eb
kishanghosh090 <kishanghosh090@gmail.com>
2026-07-18 20:58:16 +0530

    add

---
+++ rivison/conditionls.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ 10 -gt 5 ]
+then
+    echo "10"
+else 
+    echo "5"
+fi    
---
+++ rivison/user-intaraction.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p "enter you name: " name
+
+echo "length of $name -> ${#name}"
